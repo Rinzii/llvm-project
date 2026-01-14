bebe4cb5db42fe511ed5624ce1aafc83201d45ec
ianpike <ianpike98@gmail.com>
2026-01-14 17:49:59 -0500

    [libc][math] Refactor fsqrt implementation to header-only in src/__support/math folder.

--- libc/shared/math/fsqrt.h
+++ libc/shared/math/fsqrt.h
@@ -1,4 +1,4 @@
-//===-- Implementation of fsqrt function ----------------------------------===//
+//===-- Shared fsqrt function --------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/math/fsqrt.h"
+#ifndef LLVM_LIBC_SHARED_MATH_FSQRT_H
+#define LLVM_LIBC_SHARED_MATH_FSQRT_H
+
+#include "shared/libc_common.h"
 #include "src/__support/math/fsqrt.h"
 
 namespace LIBC_NAMESPACE_DECL {
+namespace shared {
 
-LLVM_LIBC_FUNCTION(float, fsqrt, (double x)) { return math::fsqrt(x); }
+using math::fsqrt;
 
+} // namespace shared
 } // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SHARED_MATH_FSQRT_H
